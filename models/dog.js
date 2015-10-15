b4c8c978bad61dc5b8032decad8b82f3198430ed
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-15 16:23:20 -0500

    set up models - isn't this a more useful comment

--- models/dog.js
+++ models/dog.js
@@ -1,1 +1,13 @@
 // Dog model w/ Mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var dogSchema = new Schema({
+    name: String,
+    picture: String,
+    age: Number,
+    occupation: String;
+    neuter: Boolean
+});
+
+module.exports = mongoose.model('Dog', dogSchema);
--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
 // User model w/ Mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var kyleSchema = new Schema({
+    name: String,
+    likes: Array
+});
+
+module.exports = mongoose.model('Kyle', kyleSchema);

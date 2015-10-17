bab26db71f9f0f3ee8f6f6c10f279b4c0ae4bc29
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-17 14:48:40 -0500

    Now when you like a dog you already liked it doesn't add to db and it tells you

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res, next) {
 			}else{
 				var i = Math.floor(Math.random()*count);
 				Dog.find({},function(err, dog){
-					res.render('index', { title: 'Express', name: dog[i]['name'], picture: dog[i]['picture'], age: dog[i]['age'], occupation: dog[i]['occupation'], neuter: dog[i]['neuter'], dog_id: dog[i]['_id'] });
+					res.render('index', { title: 'Express', name: dog[i]['name'], picture: dog[i]['picture'], age: dog[i]['age'], occupation: dog[i]['occupation'], neuter: dog[i]['neuter'], dog_id: dog[i]['_id'], response: response });
 				});
 				
 			}
@@ -34,18 +34,56 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/likely', function(req, res){
+	var like = "";
+	Kyle.find({}, function(err, kyle) {
+		for(var i=0; i<kyle[0]['likes'].length; i++) {
+			console.log(i + " " + kyle[0]['likes'][i]);
+			if (kyle[0]['likes'][i] === kyle[0]['likes'][7]) {
+				console.log("already pawed right on this dog");
+				like = i;
+			}
+			console.log("should show");
+		}
+		if (like != "") {
+			console.log("Dog already selected");
+			res.json(kyle[0]['likes'][like]);
+		} else {
+			res.json(kyle[0]['likes']);
+		}
+	});
+});
+
 /* POST when the user "likes" a new Corgi. */
 router.post('/', function(req, res, next) {
 	var id = req.body.likes;
-	var query = {"_id": '562290bee4b0f1d92b9d5fae'};
-	var update = {$push: {likes: id}};
-	var options = {new: true};
-	Kyle.findOneAndUpdate(query, update, options, function(err, person) {
-  if (err) {
-    console.log('got an error');
-  }
-});
-	res.redirect('/');
+	var like = "";
+	Kyle.find({}, function(err, kyle) {
+		for(var i=0; i<kyle[0]['likes'].length; i++) {
+			if (kyle[0]['likes'][i] === id) {
+				like = i;
+			}
+		}
+		if (like != "") {
+			Dog.findOne({_id: id}, function(err, dog){
+			response = "You already pawed right on "+dog['name'];
+			res.redirect('/');
+			})
+			
+		} else {
+			response = "";
+			var query = {"_id": '562290bee4b0f1d92b9d5fae'};
+			var update = {$push: {likes: id}};
+			var options = {new: true};
+			Kyle.findOneAndUpdate(query, update, options, function(err, person) {
+  		if (err) {
+    	console.log('got an error');
+  		}
+			});
+			res.redirect('/');
+		}
+	});
+	
 });
 
 module.exports = router;
